41c189a19fb7073edb272607eb082eaed0c1fb3f
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2016-09-05 13:43:27 +0200

    fail sur les licences

--- src/org/terramagnetica/utile/RuntimeGameException.java
+++ src/org/terramagnetica/utile/RuntimeGameException.java
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with BynarysCode. If not, see <http://www.gnu.org/licenses/>.
+along with Terra Magnetica. If not, see <http://www.gnu.org/licenses/>.
  </LICENSE> */
 
 package org.terramagnetica.utile;
