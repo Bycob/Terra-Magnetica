b8dfb3813d8e33964a7dd4e3bebb4e1c760c233a
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2016-12-22 21:46:11 +0100

    Des shaders qui fonctionnent

--- src/org/terramagnetica/opengl/engine/StdUniform.java
+++ src/org/terramagnetica/opengl/engine/StdUniform.java
@@ -3,6 +3,7 @@ package org.terramagnetica.opengl.engine;
 public interface StdUniform {
 	
 	String USE_TEXTURES = "useTextures";
+	String USE_LIGHTS = "useLights";
 	
 	public interface View {
 		String PROJECTION_MATRIX = "view.projection";
@@ -14,18 +15,19 @@ public interface StdUniform {
 	public interface Light {
 		int MAX_LIGHT = 10;
 		
-		String TYPE = "light[0].type";
-		String POSITION = "light[0].position";
-		String AMBIENT = "light[0].ambient";
-		String DIFFUSE = "light[0].diffuse";
-		String SPECULAR = "light[0].specular";
-		String ATTENUATION = "light[0].attenuation";
+		String TYPE = "light[%d].type";
+		String POSITION = "light[%d].position";
+		String AMBIENT = "light[%d].ambient";
+		String DIFFUSE = "light[%d].diffuse";
+		String SPECULAR = "light[%d].specular";
+		String ATTENUATION = "light[%d].attenuation";
 	}
 	
 	public interface Material {
 		String AMBIENT = "material.ambient";
 		String DIFFUSE = "material.specular";
 		String SPECULAR = "material.specular";
+		String SPECULAR_INTENSITY = "material.specularIntensity";
 		String SHININESS = "material.shininess";
 	}
 }
--- src/org/terramagnetica/opengl/shaders/lighting3D.fs
+++ src/org/terramagnetica/opengl/shaders/lighting3D.fs
@@ -1,13 +1,25 @@
 // Lighting shader
 #version 330 core
 
+#define true 1
+#define false 0
+
 //types de lumières
 #define MAX_LIGHT 10
-#define POINT 0;
-#define SUN 1;
-#define SPOT 2;
+#define POINT 0
+#define SUN 1
+#define SPOT 2
 
 //uniforms
+uniform bool useTextures;
+uniform bool useLights;
+
+uniform struct {
+	mat4 projection;
+	mat4 camera;
+	mat4 model;
+	vec3 cameraPosition;
+} view;
 
 uniform struct {
 	int activated;
@@ -21,13 +33,86 @@ uniform struct {
 } light[MAX_LIGHT];
 
 uniform struct {
-	//TODO
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float specularIntensity;
+	float shininess;
 } material;
 
-vec4 applyLight(int lightID, vec4 initColor) {
+in vec3 fragVert;
+in vec2 fragTexCoord;
+in vec3 fragNormal;
+in vec3 fragColor;
+
+vec4 applyLight(int lightID) {
 	if (light[lightID].activated == 0) {
-		return initColor;
+		return vec4(0);
 	}
 	
-	return initColor;
+	//Calcul des positions réelles des éléments de la scene
+    vec3 normal = normalize(transpose(inverse(mat3(view.model))) * fragNormal);
+    vec3 position = vec3(view.model * vec4(fragVert, 1));
+    vec3 lightPos = light[lightID].position; //vec3(view.model * vec4(lightPos, 1));
+
+    //Calcul du vecteur fragment->camera
+    vec3 fragToCam = normalize(view.cameraPosition - position);
+
+
+    //Calcul des caractéristiques de la lumière
+    //détermination du vecteur lumière
+    vec3 fragToLight;
+    float fragToLightLength;
+
+    if (light[lightID].type == SUN) {
+        fragToLight = - lightPos;
+    }
+    else {
+        fragToLight = position - lightPos;
+    }
+
+    fragToLightLength = length(fragToLight);
+    fragToLight /= fragToLightLength;
+
+
+    //atténuation en mode POINT_LIGHT
+    float attenuation = 1;
+    if (light[lightID].type == POINT) {
+        attenuation *= 1 / (light[lightID].attenuation.x
+        	+ fragToLightLength * light[lightID].attenuation.y
+        	+ fragToLightLength * fragToLightLength * light[lightID].attenuation.z);
+    }
+	
+	
+    //Calcul de l'effet de la lumière sur le matériau
+    
+    //intensité diffuse
+    vec3 diffuseIntensity = vec3(max(- dot(normal, fragToLight), 0));
+    diffuseIntensity *= material.diffuse * light[lightID].diffuse;
+
+    //intensité ambiente
+    vec3 ambientIntensity = vec3(1);
+    ambientIntensity *= material.ambient * light[lightID].ambient;
+
+    //intensité speculaire
+    vec3 specularIntensity = vec3(pow(max(dot(fragToCam, reflect(fragToLight, normal)), 0), material.shininess));
+    specularIntensity *= diffuseIntensity * material.specular * light[lightID].specular * material.specularIntensity;
+
+
+    //Calcul final
+    vec3 gamma = vec3(1);
+    return vec4(pow(ambientIntensity + attenuation * (diffuseIntensity + specularIntensity), gamma), 1);
+}
+
+vec4 applyLights(vec4 initColor) {
+    if (!useLights) {
+    	return initColor;
+    }
+    
+    //Application de chaque lumière
+	vec4 color = vec4(0);
+	for (int i = 0 ; i < MAX_LIGHT ; i++) {
+		color += applyLight(i);
+	}
+	return clamp(color * initColor, vec4(0), vec4(1));
 }
